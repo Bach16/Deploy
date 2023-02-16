25ac065310a08d80855b6e5f5ce7f0fad3286406
Bach16 <gwyn56717@gmail.com>
2023-02-16 13:12:50 -0500

    restructuring code

--- api/index.js
+++ api/index.js
@@ -22,15 +22,16 @@ const { conn } = require('./src/db.js');
 const {getCountriesFromApi} = require("./src/controllers/getCountries.api")
 const {PostCountriesDB} = require("./src/helpers/Countries/postCountries.db")
 const {postSeasonsDB} = require("./src/helpers/Seasons/postSeasons.db")
+require("dotenv").config()
 
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, async() => {
+  server.listen(process.env.PORT, async() => {
     const data = await getCountriesFromApi()   
     PostCountriesDB(data)
     postSeasonsDB()
 
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('%s listening at ', process.env.PORT); // eslint-disable-line no-console
   });
 });
